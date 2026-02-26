557bbfbd865fab8c498f88a8258665ddea5cd166
KIMTAEKWAN <xorhks4552@naver.com>
2026-02-26 14:04:48 +0900

    컴포넌트와 props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  console.log('props', props.title);
+  return (
+    <header>
+      <h1><a href ="/">{props.title}</a></h1>
+    </header>
+  )
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>    
+        <li><a href="/read/3">javascript</a></li>       
+      </ol>
+    </nav>
+  )
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+       <Header title="REACT"></Header>
+        <Nav></Nav>      
+       <Article title="Welcome" body="Hello, REACT!"></Article>
+       <Article title="Hi!" body="Hello, User!"></Article>
     </div>
   );
 }
